2195d51a36278e7e6106d75d27e1fac053ea2bcd
hicx76 <brentmercer@brents-MacBook-Air.local>
2017-11-29 15:25:20 -0700

    Updated Foursquare

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ let locations = [
 	{title: 'Phantom Canyon', location: { lat: 38.834232, lng: -104.824948 }, fsID: '4ab97703f964a5207e7f20e3' },
 	{title: 'Cerberus Brewing Company', location: { lat: 38.833016, lng: -104.837419 }, fsID: '57c878a1498e9ad24cf1158d'},
 	{title: 'Fieldhouse Brewing Company', location: { lat: 38.825982, lng: -104.823627 }, fsID: '5390a6b2498e4fea4875bfef'},
-	{title: 'Red Leg Brewing Company', location: { lat: 38.898022, lng: -104.841796 }, fsID: 'redlegbrewco'},
+	{title: 'Trinity Brewing Company', location: { lat: 38.897450, lng: -104.854339 }, fsID: '4b118eadf964a5203c7f23e3'},
 	{title: 'Cogstone Brewing Company', location: { lat: 38.880052, lng: -104.755481 }, fsID: '56a418f7498eb236c64f6300'},
 	{title: 'Gold Camp Brewing Company', location: { lat: 38.819571, lng: -104.823610 }, fsID: '54a5ff5e498e3dc75acd6ea4'}
 ]
@@ -17,25 +17,29 @@ let largeInfoWindow;
 let markers = [];
 let bounds;
 let service;
-let street = '';
-let city = '';
-let phone;
 
 
+
+
+// Initialize map on page load.
 function initMap() {
+
+	// Grab new map from Google Maps api.
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 38.833882, lng: -104.821363},
 		zoom: 13
 	});
 
+	// Grab new infowindow box from Google Maps api.
 	largeInfoWindow = new google.maps.InfoWindow();
-
+	
+	// Run function to create markers.
 	createMarkersForPlace();
 }
 
 
 
-// Create array of marker objects for each item in location array
+// Create array of marker objects for each item in location array.
 function createMarkersForPlace() {
 	bounds = new google.maps.LatLngBounds();
 	for (let i = 0; i < locations.length; i++) {
@@ -49,63 +53,89 @@ function createMarkersForPlace() {
 			id: locations[i].fsID
 		});
 		locations[i].marker = marker;
+		locations[i].show = true;
 		bounds.extend(marker.position);
+		
+		// Listen for click on marker, to populate infowindow
 		marker.addListener('click', function() {
 			populateInfoWindow(this, largeInfoWindow);
 		});
+
+		// Add marker to markers array
 		markers.push(marker);
+
 	}
 	map.fitBounds(bounds);
+
 }
 
-// Populate info into marker window popup
-function populateInfoWindow(marker, infowindow) {
-
-	let self = marker;
-	self.setAnimation(google.maps.Animation.BOUNCE);
-	setTimeout(function(){ 
-		marker.setAnimation(null); 
-	}, 2200);
-
-	// Make sure the infowindow isn't already opened on this marker.
-	if (infowindow.marker != marker) {
-		infowindow.setContent('');
-		infowindow.marker = marker;
-		console.log(marker);
-		fourSQ(marker);
-		infowindow.setContent('<div>' + self.title + '</div>' +
-		'<div class="content">' + self.street + "</div>" +
-        '<div class="content">' + self.city + "</div>" +
-        '<div class="content">' + self.phone + "</div></div>");
-		// Make sure the marker property is cleared if the infowindow is closed.
-		infowindow.addListener('closeclick',function(){
-			infowindow.setMarker = null;
-		});
-		infowindow.open(map, marker);
-	}
-}
-
-
+// Throw error if Google Maps api fails to load
 function mapError() {
   alert("Map did not load");
 }
 
 
-// Foursquare API
-function fourSQ(marker){
-	let fsurl = 'https://api.foursquare.com/v2/venues/' + marker.id + '&client_id=Z5WTAAN5CPS2SPIUL2QQ405SOK1PPZ314A2ZVWO5PYNVUF0E&client_secret=OO43DGDQPPP13G5ERTF2WH4IVJ4DTUILZUWVN3OFZPB0XQYX&v=20171122';
+// Populate info into marker window popup
+function populateInfoWindow(marker, infowindow) {
+	let self = marker;
 
+	// Make marker bounce 3 times or so
+	self.setAnimation(google.maps.Animation.BOUNCE);
+	setTimeout(function(){ 
+		marker.setAnimation(null); 
+	}, 2200);
+
+	// Call Foursquare api function (below)
+	fourSQ(marker, infowindow);
+
+	// Make sure the infowindow isn't already opened on this marker.
+	if (infowindow.marker != marker) {
+		infowindow.setContent('');
+		infowindow.marker = marker;
+
+		// Make sure the marker property is cleared if the infowindow is closed.
+		infowindow.addListener('closeclick',function(){
+			infowindow.setMarker = null;
+		});
+		
+	}
+}
+
+
+
+// Foursquare API
+function fourSQ(marker, infowindow){
+	// Set Foursquare API url 
+	let fsurl = 'https://api.foursquare.com/v2/venues/' + marker.id + '?client_id=Z5WTAAN5CPS2SPIUL2QQ405SOK1PPZ314A2ZVWO5PYNVUF0E&client_secret=OO43DGDQPPP13G5ERTF2WH4IVJ4DTUILZUWVN3OFZPB0XQYX&v=20171122';
+	
+	// Run Foursquare Ajax request
 	$.ajax(fsurl)
+
+	// If successful...
 	.done(function(data) {
-		let street = data.response.venue.contact.street;
-		let city = data.response.venue.contact.city;
-		let phone = data.response.venue.contact.phone;
-		//open window
+		
+		// Push Foursquare API data to marker object
+		street = data.response.venue.location.address;
+		city = data.response.venue.location.city;
+		phone = data.response.venue.contact.phone;
+
+		// Set Foursquare content to infowindow
+		infowindow.setContent(
+			'<div class="title content">' + marker.title + '</div>' + 
+			'<div class="street content">' + street + '</div>' +
+			'<div class="city content">' + city + '</div>' +
+			'<div class="phone content"> <a href=tel:' + phone + '>' + phone + '</div>'
+		);
+
+		// Open infowindow
+		infowindow.open(map, marker);
 	})
+
+	// If unsuccessful...
 	.fail(function() {
+		// Throw error
 		alert( "Foursquare API error. Please refresh page." );
 	})
-
 }
 
 
@@ -115,8 +145,6 @@ function fourSQ(marker){
 // VIEWMODEL
 function AppViewModel () {
 	var self = this;
-	console.log(self);
-	console.log(locations);
 
 	this.myObservable = ko.observableArray(locations);
 
